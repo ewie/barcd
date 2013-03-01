a3dfdbae2336762175d736065f51179954cd29d0
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-03-01 18:31:45 +0000

    fix ExtractionWorker to stop if no more images are available

--- app/src/de/tu_chemnitz/mi/barcd/app/ExtractionWorker.java
+++ app/src/de/tu_chemnitz/mi/barcd/app/ExtractionWorker.java
@@ -25,7 +25,7 @@ public class ExtractionWorker extends Worker {
     public void work()
         throws ImageProviderException
     {
-        while (!isTerminated()) {
+        while (!isTerminated() && extractor.hasMoreImages()) {
             extractor.processNextImage();
         }
     }
