5e4e58d8d12b99fc5ab4182001171e2bfd1a3f56
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-03-03 17:31:25 +0000

    add Javadoc comments in ScalingOperator

--- core/src/de/tu_chemnitz/mi/barcd/image/ScalingOperator.java
+++ core/src/de/tu_chemnitz/mi/barcd/image/ScalingOperator.java
@@ -5,16 +5,31 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 /**
+ * An operator to scale {@link BufferedImage} instances.
+ *
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
  */
 public class ScalingOperator {
+    /**
+     * Scale an image to a given width. The new scaled image height is chosen so
+     * the aspect ratio will be preserved.
+     *
+     * @param image the image to scale
+     * @param width the new image width
+     *
+     * @return the scaled image
+     */
     public BufferedImage apply(BufferedImage image, int width) {
         int height = (image.getHeight() * width) / image.getWidth();
+
         BufferedImage out = new BufferedImage(width, height, image.getType());
+
         Graphics2D g = out.createGraphics();
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+            RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g.drawImage(image, 0, 0, out.getWidth(), out.getHeight(), null);
         g.dispose();
+
         return out;
     }
 }
