fece3dadc028227317cead1a850b1686d094d8f3
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-03-03 17:53:55 +0000

    deprecate VectorField
    The VectorField class' purpose is to aid motion estimation. Because this feature is no longer required nor used but the implementation sill exists the class is now deprecated.

--- core/src/de/tu_chemnitz/mi/barcd/geometry/VectorField.java
+++ core/src/de/tu_chemnitz/mi/barcd/geometry/VectorField.java
@@ -3,35 +3,36 @@ package de.tu_chemnitz.mi.barcd.geometry;
 /**
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
  */
+@Deprecated
 public class VectorField {
     private int width;
-    
+
     private int height;
-    
+
     private int blockSize;
-    
+
     private Vector[] vectors;
-    
+
     public VectorField(int blockSize, int width, int height, Vector[] vectors) {
         this.width = width;
         this.height = height;
         this.blockSize = blockSize;
         this.vectors = vectors;
     }
-    
+
     public Vector getVectorAt(int x, int y) {
         int at = (x / blockSize) + (y / blockSize) * (width / blockSize);
         return vectors[at];
     }
-    
+
     public int getBlockSize() {
         return blockSize;
     }
-    
+
     public int getWidth() {
         return width;
     }
-    
+
     public int getHeight() {
         return height;
     }
