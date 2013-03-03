8de47145151f9520de13cccab5c31ed7ee2d7c7c
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-03-03 17:27:48 +0000

    refactor DilationOperator, add Javadoc comments

--- core/src/de/tu_chemnitz/mi/barcd/image/DilationOperator.java
+++ core/src/de/tu_chemnitz/mi/barcd/image/DilationOperator.java
@@ -1,81 +1,93 @@
 package de.tu_chemnitz.mi.barcd.image;
 
 /**
- * An operator to perform image dilation.
- * 
+ * An operator to perform image dilation with a rectangular window.
+ *
+ * TODO The implementation currently ignores pixels on the border (~ width/2 on
+ *   the left and right, ~ height/2 on the top and bottom). The effect is
+ *   negligible if the window is just a few pixels wide and high.
+ *
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
  */
 public class DilationOperator {
-    private int width;
-    private int height;
-    
+    private int windowWidth;
+    private int windowHeight;
+
     /**
-     * Create a square dilation operator with all weights set to true.
-     * 
-     * @param size the kernel width and height
+     * Create a square dilation operator with the given window width and height.
+     *
+     * @param size the window width and height
      */
     public DilationOperator(int size) {
         this(size, size);
     }
-    
+
     /**
-     * Create a rectangular dilation operator with all weights set to true.
-     * 
-     * @param width the kernel width
-     * @param height the kernel height
+     * Create a rectangular dilation operator with a given window width and
+     * height.
+     *
+     * @param width the window width
+     * @param height the window height
      */
     public DilationOperator(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-    
-    public int getWidth() {
-        return this.width;
+        windowWidth = width;
+        windowHeight = height;
     }
 
-    public int getHeight() {
-        return this.height;
-    }
-    
     /**
-     * Perform the dilation on a given image.
-     * 
-     * @param pixels the row-major pixel data to dilate
-     * @param int width the image width
-     * @param int height the image height
-     * 
-     * @return the dilation of the input image
+     * @return the width of the dilation window
+     */
+    public int getWindowWidth() {
+        return windowWidth;
+    }
+
+    /**
+     * @return the height of the dilation window
+     */
+    public int getWindowHeight() {
+        return windowHeight;
+    }
+
+    /**
+     * Perform the dilation on the given pixels.
+     *
+     * @param pixels the row-major pixel data to dilate,
+     *   expects {@code width * height} elements
+     * @param width the image width
+     * @param height the image height
+     *
+     * @return the dilated pixel data in row-major order
+     *   ({@code width * height} elements)
      */
     public int[] apply(int[] pixels, int width, int height) {
         // Border padding depending on the window size.
-        int a = this.width - this.width / 2 - 1;
-        int b = this.height - this.height / 2 - 1;
-        
+        int a = windowWidth - windowWidth / 2 - 1;
+        int b = windowHeight - windowHeight / 2 - 1;
+
         int[] out = new int[pixels.length];
-        
+
         // The column maximums of the current window. When the window moves one
         // column to the right. The maximum of the left window column will be
         // overwritten with the maximum of the right window column.
-        int[] max = new int[this.width];
-        
+        int[] max = new int[windowWidth];
+
         // Image width and height minus border.
         int w = width - a;
         int h = height - b;
-        
-        // TODO currently ignores the border
+
         for (int y = b; y < h; ++y) {
             // Set up the maximums for the current row. Calculate the column
             // maximums without the rightmost window column.
-            for (int u = 0; u < this.width - 1; ++u) {
+            for (int u = 0; u < windowWidth - 1; ++u) {
                 int k = 0;
-                for (int j = 0; j < this.height; ++j) {
+                for (int j = 0; j < windowHeight; ++j) {
                     int v = y + j - b;
                     int t = pixels[v * width + u];
                     if (t > k) k = t;
                 }
-                max[u % this.width] = k;
+                max[u % windowWidth] = k;
             }
-            
+
             // For every pixel in the current row calculate the maximum of the
             // rightmost window column and override the maximum of the leftmost
             // window column which is no longer needed. Then select the maximum
@@ -84,13 +96,13 @@ public class DilationOperator {
                 // Maximum of the rightmost window column.
                 int k = 0;
                 int u = x + a;
-                for (int j = 0; j < this.height; ++j) {
+                for (int j = 0; j < windowHeight; ++j) {
                     int v = y + j - b;
                     int t = pixels[v * width + u];
                     if (t > k) k = t;
                 }
-                max[u % this.width] = k;
-                
+                max[u % windowWidth] = k;
+
                 // Maximum of all column maximums.
                 k = 0;
                 for (int i = 0; i < max.length; ++i) {
@@ -99,7 +111,7 @@ public class DilationOperator {
                 out[x + y * width] = k;
             }
         }
-        
+
         return out;
     }
 }
