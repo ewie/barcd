b8ea37cb900dbfef15817062d6237fb5ec8bae66
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-04-29 17:06:08 +0000

    remove --frames options from command line

--- app/src/de/tu_chemnitz/mi/barcd/app/CommandLineArgumentsParser.java
+++ app/src/de/tu_chemnitz/mi/barcd/app/CommandLineArgumentsParser.java
@@ -1,11 +1,7 @@
 package de.tu_chemnitz.mi.barcd.app;
 
 import java.io.File;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.cli2.Argument;
 import org.apache.commons.cli2.CommandLine;
@@ -19,12 +15,7 @@ import org.apache.commons.cli2.option.DefaultOption;
 import org.apache.commons.cli2.option.Switch;
 import org.apache.commons.cli2.util.HelpFormatter;
 import org.apache.commons.cli2.validation.FileValidator;
-import org.apache.commons.cli2.validation.InvalidArgumentException;
 import org.apache.commons.cli2.validation.UrlValidator;
-import org.apache.commons.cli2.validation.Validator;
-
-import de.tu_chemnitz.mi.barcd.util.Range;
-import de.tu_chemnitz.mi.barcd.util.TemplatedUrlSequence;
 
 /**
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
@@ -49,38 +40,9 @@ public class CommandLineArgumentsParser {
         Options options = new Options();
         options.setJobFile((File) commandLine.getValue("--job"));
         options.setXmlSchemaUrl((URL) commandLine.getValue("--xml-schema"));
-        options.setFrameUrlSequence(createFrameUrlSequence(commandLine));
         options.setDisplay(commandLine.getSwitch("+display"));
         options.setPersist(commandLine.getSwitch("+persist"));
         return options;
-    }
-
-    public TemplatedUrlSequence createFrameUrlSequence(CommandLine commandLine) {
-        String template = (String) commandLine.getValue("TEMPLATE");
-        String tag = (String) commandLine.getValue("TAG");
-        int padding = Integer.valueOf((String) commandLine.getValue("PADDING"));
-        String rangeString = (String) commandLine.getValue("RANGE");
-        Range range = parseRange(rangeString);
-        URL url;
-        try {
-            url = new File(template).toURI().toURL();
-        } catch (MalformedURLException ex) {
-            throw new RuntimeException(ex);
-        }
-        return new TemplatedUrlSequence(url, tag, range, padding);
-    }
-
-    private Range parseRange(String s) {
-        String[] ss = s.split(":", 3);
-        switch (ss.length) {
-        case 2:
-            return new Range(Integer.valueOf(ss[0]), Integer.valueOf(ss[1]));
-        case 3:
-            return new Range(Integer.valueOf(ss[0]), Integer.valueOf(ss[1]),
-                Integer.valueOf(ss[2]));
-        default:
-            throw new RuntimeException();
-        }
     }
 
     /**
@@ -98,7 +60,6 @@ public class CommandLineArgumentsParser {
 
         Group group =
             gb.withOption(createJobFileOption())
-              .withOption(createFramesOption())
               .withOption(createXmlSchemaOption())
               .withOption(createDisplaySwitch())
               .withOption(createPersistSwitch())
@@ -148,74 +109,6 @@ public class CommandLineArgumentsParser {
               .create();
 
         return jobFileOption;
-    }
-
-    private DefaultOption createFramesOption() {
-        ArgumentBuilder ab = new ArgumentBuilder();
-        DefaultOptionBuilder ob = new DefaultOptionBuilder();
-        GroupBuilder gb = new GroupBuilder();
-
-        Validator rangeValidator = new Validator() {
-            @SuppressWarnings("rawtypes")
-            @Override
-            public void validate(List objects)
-                throws InvalidArgumentException
-            {
-                Pattern pattern = Pattern.compile("^\\d+:\\d+(:-?\\d+)?$");
-                for (Object object : objects) {
-                    String range = (String) object;
-                    Matcher matcher = pattern.matcher(range);
-                    if (!matcher.matches()) {
-                        throw new InvalidArgumentException("invalid range syntax" + range);
-                    }
-                }
-            }
-        };
-
-        Argument templateArgument =
-            ab.withName("TEMPLATE")
-              .withDescription("A templated URL identifying each frame result.")
-              .withMinimum(1)
-              .withMaximum(1)
-              .create();
-
-        Argument paddingArgument =
-            ab.withName("PADDING")
-              .withDescription("The length of the sequence number padded with zeros to the left.")
-              .withMinimum(0)
-              .withMaximum(1)
-              .withDefault(0)
-              .create();
-
-        Argument tagArgument =
-            ab.withName("TAG")
-              .withDescription("A unique substring in the templated URL that will be replaced by an integer from the given range.")
-              .withMinimum(1)
-              .withMaximum(1)
-              .create();
-
-        Argument rangeArgument =
-            ab.withName("RANGE")
-              .withDescription("The range of integers for which the template tag should be evaluated. Format: start:end[:step]")
-              .withMinimum(1)
-              .withMaximum(1)
-              .withValidator(rangeValidator)
-              .create();
-
-        Group templateGroup =
-            gb.withOption(templateArgument)
-              .withOption(tagArgument)
-              .withOption(rangeArgument)
-              .withOption(paddingArgument)
-              .create();
-
-        DefaultOption framesOption =
-            ob.withLongName("frames")
-              .withChildren(templateGroup)
-              .withRequired(true)
-              .create();
-
-        return framesOption;
     }
 
     private DefaultOption createXmlSchemaOption() {
--- app/src/de/tu_chemnitz/mi/barcd/app/Options.java
+++ app/src/de/tu_chemnitz/mi/barcd/app/Options.java
@@ -3,14 +3,11 @@ package de.tu_chemnitz.mi.barcd.app;
 import java.io.File;
 import java.net.URL;
 
-import de.tu_chemnitz.mi.barcd.util.TemplatedUrlSequence;
-
 /**
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
  */
 public class Options {
     private File jobFile;
-    private TemplatedUrlSequence frameUrlSequence;
     private URL xmlSchemaUrl;
     private boolean display;
     private boolean persist;
@@ -21,14 +18,6 @@ public class Options {
 
     public File getJobFile() {
         return jobFile;
-    }
-
-    public void setFrameUrlSequence(TemplatedUrlSequence value) {
-        frameUrlSequence = value;
-    }
-
-    public TemplatedUrlSequence getFrameUrlSequence() {
-        return frameUrlSequence;
     }
 
     public void setXmlSchemaUrl(URL value) {
