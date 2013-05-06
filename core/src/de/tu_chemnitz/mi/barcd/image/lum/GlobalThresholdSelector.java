de6c130ab0aeb21c7ea3a6d599a75e2e15007caf
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-05-06 01:27:44 +0000

    remove everything deprecated from the core

--- core/src/de/tu_chemnitz/mi/barcd/image/lum/GlobalThresholdSelector.java
+++
@@ -1,9 +0,0 @@
-package de.tu_chemnitz.mi.barcd.image.lum;
-
-/**
- * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
- */
-@Deprecated
-public interface GlobalThresholdSelector {
-    public int getThreshold(LuminanceImage source);
-}
