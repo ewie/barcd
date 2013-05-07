2c425879986e15ecd871322c8daea62ece653c99
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-05-07 01:45:46 +0000

    check if video resource may exist when opening a URL with OpenCvFileFrameReader.open()

--- core/src/de/tu_chemnitz/mi/barcd/video/OpenCvFileFrameReader.java
+++ core/src/de/tu_chemnitz/mi/barcd/video/OpenCvFileFrameReader.java
@@ -4,6 +4,12 @@
  */
 package de.tu_chemnitz.mi.barcd.video;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import com.googlecode.javacv.OpenCVFrameGrabber;
@@ -44,6 +50,12 @@ public class OpenCvFileFrameReader extends OpenCvFrameReader
     public static OpenCvFileFrameReader open(URL url)
         throws FrameReaderException
     {
+        // Check if the resource given by its URL may exist, because a
+        // non-existent resource (remote or local) would cause a fatal runtime
+        // error probably caused by JavaCV.
+        if (!resourceMayExist(url)) {
+            throw new FrameReaderException("resource does not exist");
+        }
         OpenCVFrameGrabber fg = new OpenCVFrameGrabber(url.toString());
         return new OpenCvFileFrameReader(url, fg);
     }
@@ -74,5 +86,58 @@ public class OpenCvFileFrameReader extends OpenCvFrameReader
     @Override
     public int getLengthInFrames() {
         return frameGrabber.getLengthInFrames();
+    }
+
+    /**
+     * Check if the resource given by its URL may exist.
+     *
+     * @param url
+     *
+     * @return true if the resource may exist, false if the resource definitely
+     *   does not exist
+     */
+    private static boolean resourceMayExist(URL url) {
+        String schema = url.getProtocol();
+        if (schema.equals("http")) {
+            return httpResourceMayExist(url);
+        } else if (schema.equals("file")) {
+            return resourceMayExistFile(url);
+        }
+        return true;
+    }
+
+    private static boolean httpResourceMayExist(URL url) {
+        HttpURLConnection conn;
+        try {
+            conn = (HttpURLConnection) url.openConnection();
+        } catch (IOException ex) {
+            return true;
+        }
+        try {
+            conn.setRequestMethod("HEAD");
+        } catch (ProtocolException ex) {
+            return true;
+        }
+        try {
+            conn.connect();
+        } catch (IOException ex) {
+            return true;
+        }
+        try {
+            return 200 == conn.getResponseCode();
+        } catch (IOException ex) {
+            return true;
+        }
+    }
+
+    private static boolean resourceMayExistFile(URL url) {
+        URI uri;
+        try {
+            uri = url.toURI();
+        } catch (URISyntaxException ex) {
+            return false;
+        }
+        File file = new File(uri);
+        return file.exists();
     }
 }
