c7b40ead1efd37e0c5e4535032324cbe3df09573
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-05-07 01:59:05 +0000

    refactor Main print copyright notice

--- app/src/de/tu_chemnitz/mi/barcd/app/Main.java
+++ app/src/de/tu_chemnitz/mi/barcd/app/Main.java
@@ -8,10 +8,17 @@ package de.tu_chemnitz.mi.barcd.app;
  * @author Erik Wienhold <ewie@hrz.tu-chemnitz.de>
  */
 public class Main {
-    public static void main(String[] args) {
-        CommandLineArgumentsParser parser = new CommandLineArgumentsParser();
-        Options options = parser.parse(args);
+    private static final String COPYRIGHT_NOTICE =
+        "Copyright (c) 2012-2013 Erik Wienhold & René Richter" + '\n' +
+        "Licensed under the BSD 3-Clause License." + '\n';
 
+    public static void main(String[] args) {
+        printCopyrightNotice();
+        Options options = parseCommandLineArguments(args);
+        createAndRunApplication(options);
+    }
+
+    private static void createAndRunApplication(Options options) {
         try {
             Application app = new Application(options);
             app.run();
@@ -20,5 +27,14 @@ public class Main {
         } catch (ApplicationException ex) {
             System.err.println(ex.getMessage());
         }
+    }
+
+    private static Options parseCommandLineArguments(String[] args) {
+        CommandLineArgumentsParser parser = new CommandLineArgumentsParser();
+        return parser.parse(args);
+    }
+
+    private static void printCopyrightNotice() {
+        System.out.println(COPYRIGHT_NOTICE);
     }
 }
