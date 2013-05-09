a50d878a814110fe8613c5f30880a0a073304b51
Erik Wienhold <ewie@hrz.tu-chemnitz.de>
2013-05-09 13:20:03 +0000

    add launch scripts

---
+++ app/scripts/barcd.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem Copyright (c) 2013 Erik Wienhold & René Richter
+rem Licensed under the BSD 3-Clause License.
+
+call java -jar "..\target\barcd-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar" %*
---
+++ app/scripts/barcd.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Copyright (c) 2013 Erik Wienhold & René Richter
+# Licensed under the BSD 3-Clause License.
+
+java -jar "../target/barcd-app-0.0.1-SNAPSHOT-jar-with-dependencies.jar" $@
